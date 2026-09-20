12bfa76dadb4cdb1501a4b76c65e81d33eea9150
Carlos98D <127034836+Carlos98D@users.noreply.github.com>
2026-09-20 11:53:49 -0600

    Update Google Apps Script URL in page.tsx

--- app/formulario/page.tsx
+++ app/formulario/page.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion"
 import { Sparkles, CheckCircle, Send, User, Users, Phone, Baby, Plus, Minus, AlertCircle, Ticket } from "lucide-react"
 
 // URL DE TU GOOGLE APPS SCRIPT
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyI9G1Ttkmd9AAZczFkZ7FULTLaPhK4R4lyqL35pKbVaW0Byz80s897Mp8McA_hh7Nr/exec"
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx6mNmk2KlnlMsicluXwv_h3Ilbnhc5QKJDjK6x3mOMkiegPNl_kE4T74pnzEaNM2nL/exec"
 
 const MAX_INVITADOS_TOTAL = 200
 
